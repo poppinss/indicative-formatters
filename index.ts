9c1b779772c111d93ac8806c24087e60439c3856
Harminder virk <virk.officials@gmail.com>
2019-06-11 11:51:05 +0530

    feat: add VanillaFormatter

---
+++ index.ts
@@ -0,0 +1,14 @@
+/**
+ * @module indicative-formatters
+ */
+
+/*
+* indicative-formatters
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+export { VanillaErrorNode, VanillaFormatter } from './src/VanillaFormatter'
---
+++ src/VanillaFormatter.ts
@@ -0,0 +1,57 @@
+/**
+ * @module indicative-formatters
+ */
+
+/*
+ * indicative-formatters
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { ErrorFormatterContract } from 'indicative-compiler'
+import { ParsedRule } from 'indicative-parser'
+
+/**
+ * Shape of error node
+ */
+export type VanillaErrorNode = {
+  error: string,
+  validation: string,
+  field: string,
+}
+
+/**
+ * Vanilla formatter is a plain formatter to collect validation
+ * errors
+ */
+export class VanillaFormatter implements ErrorFormatterContract<VanillaErrorNode> {
+  public errors: VanillaErrorNode[] = []
+
+  /**
+   * Adds error to the list of existing errors
+   */
+  public addError (error: Error | string, field: string, rule: ParsedRule): void {
+    let message = ''
+    let validation = rule.name
+
+    if (error instanceof Error) {
+      message = error.message
+      validation = 'ENGINE_EXCEPTION'
+    } else {
+      message = error
+    }
+
+    this.errors.push({ error: message, validation, field })
+  }
+
+  /**
+   * Returns an array of errors or `null` when there are no
+   * errors
+   */
+  public toJSON (): VanillaErrorNode[] | null {
+    return this.errors.length ? this.errors : null
+  }
+}
